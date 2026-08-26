dbe5c1094b8c13213820594fdc60c78c935fbbad
MIN <alsgur9865@gmail.com>
2026-08-26 09:38:38 +0900

    feat: add sample trade data

---
+++ src/data/sampleTrades.js
@@ -0,0 +1,7 @@
+export const sampleTrades = [
+  { symbol: 'ALPHA', buyPrice: 51000, sellPrice: 43800, preRisePct: 18.4, holdingDays: 22, source: 'community' },
+  { symbol: 'BETA', buyPrice: 32700, sellPrice: 29900, preRisePct: 13.1, holdingDays: 17, source: 'youtube' },
+  { symbol: 'GAMMA', buyPrice: 18400, sellPrice: 21200, preRisePct: 2.6, holdingDays: 5, source: 'analysis' },
+  { symbol: 'DELTA', buyPrice: 76500, sellPrice: 70100, preRisePct: 11.8, holdingDays: 15, source: 'community' },
+  { symbol: 'EPSILON', buyPrice: 24300, sellPrice: 27900, preRisePct: 3.2, holdingDays: 4, source: 'news' }
+];
