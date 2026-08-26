cd3343ff83cb0f8cd9591528536dc1426ccc5c45
MIN <alsgur9865@gmail.com>
2026-08-26 09:38:16 +0900

    feat: add Mission 6 routes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Diagnosis from './pages/Diagnosis';
+import Analyzing from './pages/Analyzing';
+import Report from './pages/Report';
+import Reports from './pages/Reports';
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/diagnosis" element={<Diagnosis />} />
+      <Route path="/analyzing" element={<Analyzing />} />
+      <Route path="/report/:id" element={<Report />} />
+      <Route path="/reports" element={<Reports />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
